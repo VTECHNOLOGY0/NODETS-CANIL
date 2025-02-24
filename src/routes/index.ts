d8338ce2c1f9a4c4c59167dc4d87927ce10d6b05
VTECHNOLOGY0 <vt.technology0@gmail.com>
2025-02-24 01:55:55 -0300

    Final project 1.0

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,13 @@ import * as SearchController from '../controllers/searchController';
 
 const router = express.Router();
 
+
 router.get('/', PageController.home);
 router.get('/cachorros', PageController.dogs);
 router.get('/gatos', PageController.cats);
 router.get('/peixes', PageController.fishes);
 
-router.get('/pesquisa', SearchController.search);
+router.get('/search', SearchController.search);
+
 
 export default router;
